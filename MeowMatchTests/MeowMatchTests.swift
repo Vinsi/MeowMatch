9cb8cec03dffab5a639e8a070e30dc25d6bcc0f6
vinsi <winc_leo@yahoo.com>
2025-01-24 21:13:52 +0000

    Initial Commit

---
+++ MeowMatchTests/MeowMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeowMatchTests.swift
+//  MeowMatchTests
+//
+//  Created by Vinsi on 24/01/2025.
+//
+
+import Testing
+@testable import MeowMatch
+
+struct MeowMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
